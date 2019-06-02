3c95199de29985538701496f97fa52d1093b1ee2
Milad <51293408+milad-93@users.noreply.github.com>
2019-06-02 18:34:17 +0200

    Update databasConnection.php
    hidden

--- HemsidaEximination/databasConnection.php
+++ HemsidaEximination/databasConnection.php
@@ -1,6 +1,6 @@
 <?php
 $dsn = "mysql:host=localhost;dbname=mima0019;charset=utf8";
-$db = new PDO($dsn, "mima0019", "S12hiraz!");
+$db = new PDO($dsn, "Hidden", "Hidden);
 
 
 //pdo till log in och register
